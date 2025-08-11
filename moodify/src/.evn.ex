38930fac51e80ae3c203520705307bb5d128c541
basharlevin <basharaboalneaj@outlook.com>
2025-08-11 17:47:08 +0200

    ex env

---
+++ moodify/src/.evn.ex
@@ -0,0 +1,4 @@
+VITE_SPOTIFY_CLIENT_ID=YOUR_CLIENT_ID
+VITE_SPOTIFY_REDIRECT_URI=http://127.0.0.1:5173/callback
+VITE_SPOTIFY_AUTH_ENDPOINT=https://accounts.spotify.com/authorize
+VITE_SPOTIFY_SCOPES=user-read-recently-played user-top-read
